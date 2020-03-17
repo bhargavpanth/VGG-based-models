6a0bae586db2c2cc371eefe3cc198d5bceadf755
Bhargav Panth <bhargavrpanth@gmail.com>
2020-03-17 00:36:48 +0000

    added VGG19 model - model takes way too long to train on cats vs dogs

--- vgg19.py
+++ vgg19.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from model import CNN_Model
 
-class VGG_16(CNN_Model):
+class VGG_19(CNN_Model):
 
     # Set trainable to True if you want the model to drop all
     # its pre trained weights and biases
@@ -12,27 +12,27 @@ class VGG_16(CNN_Model):
         self.trainable = trainable
 
     def model(self):
-        return tf.keras.applications.vgg16.VGG16()
+        return tf.keras.applications.vgg19.VGG19()
 
     def __sequential(self, output_classes: int):
-        non_sequential_vgg_16_model = self.model()
-        sequential_vgg_16_model = Sequential()
+        non_sequential_vgg_19_model = self.model()
+        sequential_vgg_19_model = Sequential()
 
-        for layer in non_sequential_vgg_16_model.layers[:-1]:
+        for layer in non_sequential_vgg_19_model.layers[:-1]:
             layer.trainable = self.trainable
-            sequential_vgg_16_model.add(layer)
-
+            sequential_vgg_19_model.add(layer)
+        
         # adding output layer with the required number of output classes
-        sequential_vgg_16_model.add(Dense(units=output_classes, activation='softmax'))
-        return sequential_vgg_16_model
+        sequential_vgg_19_model.add(Dense(units=output_classes, activation='softmax'))
+        return sequential_vgg_19_model
 
     def build(self, output_classes: int):
         return self.__sequential(output_classes)
 
 # To compile the model
 
-# vgg_16_model = VGG_16().build(2)
-# vgg_16_model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy']) 
+# vgg_19_model = VGG_19().build(2)
+# vgg_19_model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy']) 
 
 # To run it on training set
-# vgg_16_model.fit()
+# vgg_19_model.fit()
